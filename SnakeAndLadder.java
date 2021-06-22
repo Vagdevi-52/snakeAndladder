192546feee67403bb4a31459851d39dee076af9e
vagdevi-52 <dhulipalavagdevi1952@email.com>
2021-06-22 10:56:29 -0700

    dice roll

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -5,6 +5,8 @@ public static void main(String[] args)
 System.out.println("welcome to snake and ladder");
 int playerposition=0;
 System.out.println("player position is : "+playerposition);
+int die = (int)Math.floor(Math.random() *10) % 7;
+System.out.println("die rolled is :" +die);
 }
 }
 
