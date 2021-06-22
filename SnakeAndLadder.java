ab91a34f40b9f0f0b3b4711b831d2d9e6c4d3efc
vagdevi-52 <dhulipalavagdevi1952@email.com>
2021-06-22 12:30:14 -0700

    Dicecount

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,35 +1,40 @@
 import java.util.Scanner;
+
 public class SnakeAndLadder
 {
-public static void main(String[] args)
-{
-int playerposition=0;
-int winningposition=100;
-System.out.println("welcome to snake and ladder");
-while(winningposition>playerposition)
-{
-int die = (int)Math.floor(Math.random() *10) % 7;
-System.out.println("die rolled is :" +die);
-int remainingposition=winningposition-playerposition;
-int option =(int) (Math.floor(Math.random() * 10)%3);
-if(remainingposition>=die)
-switch(option)
-{
-case 0:
-System.out.println("No play " +0);
-playerposition += 0;
-break;
-case 1:
-System.out.println("Ladder" +die);
-playerposition +=die;
-break;
-default: System.out.println("snake" +die);
-playerposition -=die;
-if(playerposition<0)
-playerposition=-0;
+ public static void main(String[] args)
+ {
+   int playerposition=0;
+   int winningposition=100;
+   int Diecount=0;
+   System.out.println("welcome to snake and ladder");
+   while(winningposition>playerposition)
+   {  
+        int die = (int)Math.floor(Math.random() *10) % 7;
+        Diecount++;
+        System.out.println("die rolled is :" +die);
+        int remainingposition=winningposition-playerposition;
+        int option =(int) (Math.floor(Math.random() * 10)%3);
+        if(remainingposition>=die)
+        {
+        switch(option)
+        {
+        case 0:
+        System.out.println("No play " +0);
+        playerposition += 0;
+        break;
+        case 1:
+        System.out.println("Ladder" +die);
+        playerposition +=die;
+        break;
+        default: System.out.println("snake" +die);
+        playerposition -=die;
+        if(playerposition<0)
+        playerposition=-0;
+    }
+ System.out.println("player position is : "+playerposition);
+ System.out.println("player position is : "+playerposition);
 }
-
-System.out.println("player position is : "+playerposition);
 }
 }
 }
