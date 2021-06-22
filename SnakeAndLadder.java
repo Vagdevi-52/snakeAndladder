05c2397fa574d4b6aea3a79a6f729973cd343ae5
vagdevi-52 <dhulipalavagdevi1952@email.com>
2021-06-22 10:31:37 -0700

    intial commit

---
+++ SnakeAndLadder.java
@@ -0,0 +1,10 @@
+public class SnakeAndLadder
+{
+public static void main(String[] args)
+{
+System.out.println("welcome to snake and ladder");
+int playerposition=0;
+System.out.println("player position is : "+playerposition);
+}
+}
+
